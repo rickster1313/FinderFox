2bc14cea7133145e3c81bc7170a5fd6d6adb2623
gsouzao <gustavoas034@gmail.com>
2018-06-15 11:48:48 -0300

    começando a home do site, com um cabeçalho

--- views/home.php
+++ views/home.php
@@ -1,16 +1,35 @@
 <!DOCTYPE HTML>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="utf-8">
 	<title>FinderFox</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/bootstrap-grid.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/bootstrap-reboot.min.css">
 </head>
 	
 <body>
-	<h1>Aqui é a página inicial</h1>
-	<a href="<?php echo BASE_URL; ?>"><button>Página Inicial</button></a>
-	<a href="<?php echo BASE_URL; ?>login"><button>login</button></a>
-	<a href="<?php echo BASE_URL; ?>cadastro"><button>cadastro</button></a>
-	<hr>	
+    
+    <div class="container-fluid" style="padding: 0px;">
+	    <div class="jumbotron">
+	    	<div class="botoes" style="">
+	 	 		<a href="<?php echo BASE_URL; ?>login"><button class="btn btn-outline-dark ">login</button></a>
+				<a href="<?php echo BASE_URL; ?>cadastro"><button class="btn btn-outline-dark">cadastro</button></a>
+			</div>
+	    	<img src="">
+	 	 	<h1 class="text-center">FinderFox</h1> 
+	 	 	<p class="text-center">Gerenciando seu negócio e vendendo pastel de flango na esquina do alberto</p>
+	 	 	<input class="form-control mx-auto d-block" id="myInput" type="text" placeholder="Pesquise o AV desejado..." style="width:40%;">
+	 	 	
+		</div>
+	</div>
+
+	<script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/bootstrap.bundle.min.js"></script>
+
+
 
 </body>
 </html>
