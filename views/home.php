c84f3d2deca720577fb69b3f2248147a82da3562
Luciano Dias <lucianodias870@gmail.com>
2018-06-16 11:54:42 -0300

    Criando menu da home

--- views/home.php
+++ views/home.php
@@ -7,6 +7,8 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/bootstrap-grid.min.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/bootstrap-reboot.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/component.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/normalize.css">
 </head>
 	
 <body>
@@ -23,11 +25,32 @@
 	 	 	<input class="form-control mx-auto d-block" id="myInput" type="text" placeholder="Pesquise o AV desejado..." style="width:40%;">
 	 	 	
 		</div>
+		<!-- Grey with black text -->
+<nav class="navbar navbar-expand-sm bg-light navbar-light justify-content-center" style="margin-top:-31.5px;">
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="#">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Sobre</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Beneficios</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Suporte</a>
+    </li>
+  </ul>
+</nav>
+
 	</div>
+
 
 	<script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/jquery-3.3.1.min.js"></script>
 	<script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/bootstrap.bundle.min.js"></script>
+	<script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/modernizr.custom.js"></script>
+	
 
 
 
