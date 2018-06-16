8d380fd0854f6e76622a97be37d619d4c107fc28
gsouzao <gustavoas034@gmail.com>
2018-06-16 18:52:36 -0300

    paginas ancoradas e efeito no menu

--- views/home.php
+++ views/home.php
@@ -1,7 +1,9 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 	<meta charset="utf-8">
 	<title>FinderFox</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/bootstrap.min.css">
@@ -9,41 +11,71 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/bootstrap-reboot.min.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/component.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/normalize.css">
+
+<style type="text/css">
+  
+</style>
+
 </head>
 	
 <body>
     
+    
     <div class="container-fluid" style="padding: 0px;">
 	    <div class="jumbotron">
 	    	<div class="botoes" style="">
-	 	 		<a href="<?php echo BASE_URL; ?>login"><button class="btn btn-outline-dark ">login</button></a>
-				<a href="<?php echo BASE_URL; ?>cadastro"><button class="btn btn-outline-dark">cadastro</button></a>
+	 	 		<a href="<?php echo BASE_URL; ?>login"><button class="btn btn-outline-dark ">LOGIN</button></a>
+				<a href="<?php echo BASE_URL; ?>cadastro"><button class="btn btn-outline-dark">CADASTRO</button></a>
 			</div>
 	    	<img src="">
 	 	 	<h1 class="text-center">FinderFox</h1> 
 	 	 	<p class="text-center">Gerenciando seu negócio e vendendo pastel de flango na esquina do alberto</p>
 	 	 	<input class="form-control mx-auto d-block" id="myInput" type="text" placeholder="Pesquise o AV desejado..." style="width:40%;">
-	 	 	
 		</div>
-		<!-- Grey with black text -->
-<nav class="navbar navbar-expand-sm bg-light navbar-light justify-content-center" style="margin-top:-31.5px;">
-  <ul class="navbar-nav">
+  
+
+    <section class="color-6">
+<nav class="navbar navbar-expand-sm bg-light navbar-light justify-content-center cl-effect-3" style="margin-top:-31.5px;">
+  <ul class="navbar-nav nav">
     <li class="nav-item active">
-      <a class="nav-link" href="#">Home</a>
+      <a class="nav-link" href="#Home" data-toggle="tab">Home</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#">Sobre</a>
+      <a class="nav-link" href="#Sobre" data-toggle="tab">Sobre</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#">Beneficios</a>
+      <a class="nav-link" href="#Beneficios" data-toggle="tab">Beneficios</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#">Suporte</a>
+      <a class="nav-link" href="#Suporte" data-toggle="tab">Suporte</a>
     </li>
   </ul>
 </nav>
+</section>
+</div>
 
-	</div>
+<div class="container">
+
+<div class="tab-content">
+<div id="Home" class="tab-pane in active">HOME
+
+</div>
+
+<div id="Sobre" class="tab-pane fade">
+  <h2>SOBRE NÓS</h2>
+</div>
+
+<div id="Beneficios" class="tab-pane fade">BENEFICIOS
+</div>
+
+<div id="Suporte" class="tab-pane fade">SUPORTE</div>
+</div>
+
+</div>
+
+
+
+
 
 
 	<script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/jquery-3.3.1.min.js"></script>
