eb8c1664787e9342e3d431520fc7fd025f6e730c
gsouzão <gustavoas034@gmail.com>
2018-06-18 10:51:05 -0300

    painel part 1 pagina empresarios

--- views/empresario.php
+++ views/empresario.php
@@ -10,14 +10,57 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/bootstrap-reboot.min.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/component.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/normalize.css">
+
+	<style type="text/css">
+		.sidenav {
+    height: 100%;
+    width: 160px;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: #111;
+    overflow-x: hidden;
+    padding-top: 20px;
+}
+
+.sidenav a {
+    padding: 6px 8px 6px 16px;
+    text-decoration: none;
+    font-size: 25px;
+    color: #818181;
+    display: block;
+}
+
+.sidenav a:hover {
+    color: #f1f1f1;
+}
+
+
+@media screen and (max-height: 450px) {
+    .sidenav {padding-top: 15px;}
+    .sidenav a {font-size: 18px;}
+}
+	</style>
 	
 </head>
 <body>
 
-	<h1>Olá <?php $newNome = explode(" ", $nome);echo strtoupper($newNome[0]);?></h1>
-	<a href="<?php echo BASE_URL ?>login/deslogar"><button id="btn_sair">SAIR</button></a>
+	<div class="container-fluid">
+	<div class="sidenav">
+  <a href="#about">About</a>
+  <a href="#services">Services</a>
+  <a href="#clients">Clients</a>
+  <a href="#contact">Contact</a>
+</div>
+</div>
 
+<div class="container">
 
+<h1 class="float-left">Olá <?php $newNome = explode(" ", $nome);echo strtoupper($newNome[0]);?></h1>
+	<a href="<?php echo BASE_URL ?>login/deslogar"><button class="btn btn-outline-dark" id="btn_sair">SAIR</button></a>
+
+</div>
 
 	<script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/jquery-3.3.1.min.js"></script>
 	<script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/bootstrap.min.js"></script>
