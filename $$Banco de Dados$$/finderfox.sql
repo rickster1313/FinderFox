f1ad9c5cc774a92203555bdb10cde7e9a82e5041
riquelme <riquelme13102001@gmail.com>
2018-06-17 21:30:26 -0300

    adicionando o BD atualizado

--- $$Banco de Dados$$/finderfox.sql
+++ $$Banco de Dados$$/finderfox.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 10-Maio-2018 às 07:47
+-- Generation Time: 18-Jun-2018 às 02:27
 -- Versão do servidor: 10.1.22-MariaDB
 -- PHP Version: 7.1.4
 
@@ -35,8 +35,7 @@ CREATE TABLE `tbl_usuarios` (
   `nvl_acesso` varchar(50) NOT NULL,
   `email` varchar(220) NOT NULL,
   `senha` varchar(220) NOT NULL,
-  `CPF` varchar(220) DEFAULT NULL,
-  `CNPJ` varchar(220) DEFAULT NULL,
+  `CPF_CNPJ` varchar(220) DEFAULT NULL,
   `endereco` varchar(220) NOT NULL,
   `telefone` varchar(220) NOT NULL,
   `sexo` varchar(220) NOT NULL,
@@ -49,8 +48,10 @@ CREATE TABLE `tbl_usuarios` (
 -- Extraindo dados da tabela `tbl_usuarios`
 --
 
-INSERT INTO `tbl_usuarios` (`id`, `nome`, `data_nasc`, `nvl_acesso`, `email`, `senha`, `CPF`, `CNPJ`, `endereco`, `telefone`, `sexo`, `status`, `created`, `modified`) VALUES
-(1, 'Riquelme Daniel de Lima', '2001-10-13', 'adm', 'riquelme13102001@gmail.com', '$2y$10$cV1FZ2VBDSQY4E6xcElTseGmudF5fqysY8bVIkcbYJlG6M5Gs/Jpy', '34776509806', NULL, 'rua uruguai 85', '011996738748', 'Masculino', 0, '2018-04-14 15:32:00', NULL);
+INSERT INTO `tbl_usuarios` (`id`, `nome`, `data_nasc`, `nvl_acesso`, `email`, `senha`, `CPF_CNPJ`, `endereco`, `telefone`, `sexo`, `status`, `created`, `modified`) VALUES
+(1, 'Riquelme Daniel de Lima', '2001-10-13', 'adm', 'riquelme13102001@gmail.com', '$2y$10$cV1FZ2VBDSQY4E6xcElTseGmudF5fqysY8bVIkcbYJlG6M5Gs/Jpy', '347.765.098-06', 'rua uruguai 85', '(11) 99673-8748', 'Masculino', 0, '2018-04-14 15:32:00', NULL),
+(2, 'fulano01', '2018-05-31', 'empresario', 'fulano01@gmail.com', '$2y$10$9HTaC90xohEtFyZyCoa1lOEFPUlMfpNoUR.6UXpeY/lP80RRYh.ry', '347.765.098-05', 'rua do fulano 01', '(13) 13131-6641', 'Feminino', 0, '2018-05-31 04:28:53', NULL),
+(3, 'fulano02', '2018-05-31', 'empresario', 'fulano02@gmail.com', '$2y$10$cSqx4475VjlWNGknJa98FuG7wCgI9aqC0nvwRBKSb1UOYvws3H.ky', '12.345.678/9123-45', 'fasdfwerfewf', '(23) 42423-4242', 'Outro', 0, '2018-05-31 05:22:36', NULL);
 
 --
 -- Indexes for dumped tables
@@ -70,7 +71,7 @@ ALTER TABLE `tbl_usuarios`
 -- AUTO_INCREMENT for table `tbl_usuarios`
 --
 ALTER TABLE `tbl_usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
