b5351bb59e2d8595666e16d4ad1f502567106583
riquelme <riquelme13102001@gmail.com>
2018-06-24 22:50:33 -0300

    arrumando ulr do servidor statica

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php 
 	GLOBAL $pdo;
-	define("BASE_URL","http://localhost/finderfox/");
+	define("BASE_URL","http://".$_SERVER['SERVER_NAME']."/finderfox/");
 	try {
 		$pdo = new PDO("mysql:dbname=finderfox;host=localhost", "root", "");
 	} catch (PDOException $e) {
