52ae57b99c85e44858fcbeb9d02174d549fc1484
riquelme <riquelme13102001@gmail.com>
2018-06-28 18:00:43 -0300

    comecando a desgraca

--- assets/js/home.js
+++ assets/js/home.js
@@ -4,6 +4,20 @@ var $j = jQuery.noConflict();
 			$j('#home_p').trigger('click');
 		});
 
+		function mostraAV(valor){
+			if(nums == 1){
+				$j("#resultado_av").append("<div id = 'linha_av"+collum+"' class='row justify-content-center justify-content-around align-content-around' style='padding-top:30px'> <div class='col-md-3 bg-light' style='height: 130px'><div class='logo' style='height: 130px; width:30%; background-color: #666'><img src = ' assets/images/AV/"+valor.logo_av+"'></div> </div>");
+			}else if(nums<3){
+				$j("#resultado_av #linha_av"+collum).append("<div class='col-md-3 bg-light' style='height: 130px'><div class='logo' style='height: 130px; width:30%; background-color: #666'><img src = 'assets/images/AV/"+valor.logo_av+"'></div>");
+				
+			}else{
+				$j("#resultado_av #linha_av"+collum).append("<div class='col-md-3 bg-light' style='height: 130px'><div class='logo' style='height: 130px; width:30%; background-color: #666'><img src = ' assets/images/AV/"+valor.logo_av+"'></div>");
+				nums = 0;
+				collum++;
+			}
+			nums++;
+		}
+
 		function pesquisaAV(){ 
 			var dados = $j("#myInput").serialize();
 			$j.ajax({
@@ -12,7 +26,16 @@ var $j = jQuery.noConflict();
 			    data:dados,
 			    dataType:'json',
 			    success:function(json){
-			       json.forEach(mostraAV);
+			        if(json.length > 0){
+			        	$j("#resultado_av").html("");
+			        	nums = 1;
+			        	collum = 1;
+			        	json.forEach(mostraAV);
+
+			        }else{
+			        	$j("#resultado_av").html("NENHUM AV ENCONTRADO");
+			        }
+			        
 			    },
 			    error:function(){
 			    	console.log("error no ajax");
