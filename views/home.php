cd1a7f0655173146ac2400947c91ca39731e99e1
riquelme <riquelme13102001@gmail.com>
2018-06-29 18:34:30 -0300

    gg

--- views/home.php
+++ views/home.php
@@ -106,29 +106,7 @@
 <div id="Home" class="tab-pane in active">
   <div class="ambiente" id="resultado_av" style="width: 100%; height: 730px; background: rgba(0,0,0,0.6); ">
     <br>
-    <?php  ?>
 
-    <div class="row justify-content-center justify-content-around align-content-around">
-      <div class="col-md-3 bg-light" style="height: 130px;">
-        <div class="logo" style="height: 130px; width:30%; background-color: #666;">
-          
-        </div>
-      </div>
-
-      <div class="col-md-3 bg-primary" style="height: 130px;">
-        <div class="logo" style="height: 130px; width:30%; background-color: #666;">
-          
-        </div>
-      </div>
-
-      <div class="col-md-3 bg-success" style="height: 130px;">
-        <div class="logo" style="height: 130px; width:30%; background-color: #666;">
-          
-        </div>
-      </div>
-    </div>
-
-    <?php ?>
      <br>
     
 
