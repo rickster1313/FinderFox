bae2af5cecf7cf81b5bd570e20bf3a2870bde9e7
rickster1313 <riquelme13102001@gmail.com>
2018-08-03 09:11:47 -0300

    salvando bd

--- $$Banco de Dados$$/finderfox.sql
+++ $$Banco de Dados$$/finderfox.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 01-Ago-2018 às 07:05
--- Versão do servidor: 10.1.22-MariaDB
--- PHP Version: 7.1.4
+-- Generation Time: 03-Ago-2018 às 14:11
+-- Versão do servidor: 10.1.19-MariaDB
+-- PHP Version: 5.5.38
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -40,14 +38,6 @@ CREATE TABLE `tbl_avs` (
   `created` varchar(220) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `tbl_avs`
---
-
-INSERT INTO `tbl_avs` (`id_av`, `user_id`, `url`, `nome_av`, `slogan_av`, `logo_av`, `categoria`, `func`, `created`) VALUES
-(3, 2, 'fulanozagem-mecanica', 'fulanozagem mecanica', 'fulano mecanicas', 'logo-fulanozagem-mecanica.jpeg', '', '', ''),
-(4, 3, 'zed', 'zed', 'a lÃ¢mina maldita ceifarÃ¡e a sombra viva e morta faz o justo ao falso cognatoa lÃ¢mina maldita ceifarÃ¡e a sombra viva e morta faz o justo ao falso cognato', 'logo-zed.png', '', '', '');
-
 -- --------------------------------------------------------
 
 --
@@ -59,9 +49,21 @@ CREATE TABLE `tbl_endereco` (
   `user_id` int(11) NOT NULL,
   `cep` varchar(50) NOT NULL,
   `rua` varchar(220) NOT NULL,
+  `estado` varchar(20) NOT NULL,
   `cidade` varchar(220) NOT NULL,
-  `numero` int(11) NOT NULL
+  `numero` varchar(220) NOT NULL,
+  `nome` varchar(220) NOT NULL,
+  `active` int(11) DEFAULT NULL,
+  `lat` varchar(50) DEFAULT NULL,
+  `lon` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `tbl_endereco`
+--
+
+INSERT INTO `tbl_endereco` (`id_end`, `user_id`, `cep`, `rua`, `estado`, `cidade`, `numero`, `nome`, `active`, `lat`, `lon`) VALUES
+(2, 6, '13238-110', 'Rua das AmÃ©ricas', 'SP', 'Campo Limpo Paulista', '304', 'principal', 0, '', '');
 
 -- --------------------------------------------------------
 
@@ -87,9 +89,7 @@ CREATE TABLE `tbl_usuarios` (
 --
 
 INSERT INTO `tbl_usuarios` (`id`, `nome`, `nvl_acesso`, `email`, `senha`, `CPF_CNPJ`, `telefone`, `status`, `created`, `modified`) VALUES
-(1, 'Riquelme Daniel de Lima', 'adm', 'riquelme13102001@gmail.com', '$2y$10$cV1FZ2VBDSQY4E6xcElTseGmudF5fqysY8bVIkcbYJlG6M5Gs/Jpy', '34776509806', '011996738748', 0, '2018-04-14 15:32:00', NULL),
-(2, 'Fulano01', 'empresario', 'fulano01@gmail.com', '$2y$10$s83SCpLJ/azIugqq8TzI3.Yg3HoIAyaITBpz0WDbKLRnD7h0N1kIy', '123.456.789-11', '(11) 98756-4321', 0, '2018-06-20 08:07:45', NULL),
-(3, 'tester empresario', 'empresario', 'empre', '$2y$10$khKt4tZtq.hOFh4wfhBg3.Jd4KLVkuC1gOc/Ti1.ei0Kj.szbE8kO', '12.345.678/9909-87', '(12) 34567-8909', 0, '2018-06-20 11:14:33', NULL);
+(6, 'empresario', 'empresario', 'empre', '$2y$10$ur7mbGCxpLnNEirAYnI2ue0C3wra.TuH97l30fg5OGlxXi.MH5wcy', '23.423.452/2342-34', '(67) 567567565', 0, '2018-08-03 08:22:05', NULL);
 
 --
 -- Indexes for dumped tables
@@ -122,20 +122,16 @@ ALTER TABLE `tbl_usuarios`
 --
 ALTER TABLE `tbl_avs`
   MODIFY `id_av` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
 --
 -- AUTO_INCREMENT for table `tbl_endereco`
 --
 ALTER TABLE `tbl_endereco`
-  MODIFY `id_end` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `id_end` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `tbl_usuarios`
 --
 ALTER TABLE `tbl_usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-COMMIT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
