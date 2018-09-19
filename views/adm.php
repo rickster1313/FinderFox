973dfccebec6b10d7bce3b4d0a06c941d6c7bb42
rickster1313 <riquelme13102001@gmail.com>
2018-09-19 14:06:21 -0300

    add adm painel

--- views/adm.php
+++ views/adm.php
@@ -13,25 +13,25 @@
     <title>Dashboard</title>
 
     <!-- Fontfaces CSS-->
-    <link href="css/font-face.css" rel="stylesheet" media="all">
-    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
-    <link href="vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
-    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="<?php echo BASE_URL; ?>assets/css/font-face.css" rel="stylesheet" media="all">
+    <link href="<?php echo BASE_URL; ?>assets/vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <link href="<?php echo BASE_URL; ?>assets/vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+    <link href="<?php echo BASE_URL; ?>assets/vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
 
     <!-- Bootstrap CSS-->
-    <link href="vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
+    <link href="<?php echo BASE_URL; ?>assets/vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
 
     <!-- Vendor CSS-->
-    <link href="vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
-    <link href="vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
-    <link href="vendor/wow/animate.css" rel="stylesheet" media="all">
-    <link href="vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
-    <link href="vendor/slick/slick.css" rel="stylesheet" media="all">
-    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
-    <link href="vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
+    <link href="<?php echo BASE_URL; ?>assets/vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
+    <link href="<?php echo BASE_URL; ?>assets/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
+    <link href="<?php echo BASE_URL; ?>assets/vendor/wow/animate.css" rel="stylesheet" media="all">
+    <link href="<?php echo BASE_URL; ?>assets/vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
+    <link href="<?php echo BASE_URL; ?>assets/vendor/slick/slick.css" rel="stylesheet" media="all">
+    <link href="<?php echo BASE_URL; ?>assets/vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="<?php echo BASE_URL; ?>assets/vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
 
     <!-- Main CSS-->
-    <link href="css/theme.css" rel="stylesheet" media="all">
+    <link href="<?php echo BASE_URL; ?>assets/css/theme.css" rel="stylesheet" media="all">
 
 </head>
 
@@ -43,7 +43,7 @@
                 <div class="container-fluid">
                     <div class="header-mobile-inner">
                         <a class="logo" href="index.html">
-                            <img src="images/icon/logo.png" alt="CoolAdmin" />
+                            <img src="<?php echo BASE_URL; ?>assets/images/logo.png" alt="CoolAdmin" />
                         </a>
                         <button class="hamburger hamburger--slider" type="button">
                             <span class="hamburger-box">
@@ -145,7 +145,7 @@
         <aside class="menu-sidebar d-none d-lg-block">
             <div class="logo">
                 <a href="#">
-                    <img src="images/icon/logo.png" alt="Cool Admin" />
+                    <img src="<?php echo BASE_URL; ?>assets/images/logo.png" alt="Cool Admin" />
                 </a>
             </div>
             <div class="menu-sidebar__content js-scrollbar1">
@@ -961,28 +961,28 @@
     </div>
 
     <!-- Jquery JS-->
-    <script src="vendor/jquery-3.2.1.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>assets/vendor/jquery-3.2.1.min.js"></script>
     <!-- Bootstrap JS-->
-    <script src="vendor/bootstrap-4.1/popper.min.js"></script>
-    <script src="vendor/bootstrap-4.1/bootstrap.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>assets/vendor/bootstrap-4.1/popper.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>assets/vendor/bootstrap-4.1/bootstrap.min.js"></script>
     <!-- Vendor JS       -->
-    <script src="vendor/slick/slick.min.js">
+    <script src="<?php echo BASE_URL; ?>assets/vendor/slick/slick.min.js">
     </script>
-    <script src="vendor/wow/wow.min.js"></script>
-    <script src="vendor/animsition/animsition.min.js"></script>
-    <script src="vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+    <script src="<?php echo BASE_URL; ?>assets/vendor/wow/wow.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>assets/vendor/animsition/animsition.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>assets/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
     </script>
-    <script src="vendor/counter-up/jquery.waypoints.min.js"></script>
-    <script src="vendor/counter-up/jquery.counterup.min.js">
+    <script src="<?php echo BASE_URL; ?>assets/vendor/counter-up/jquery.waypoints.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>assets/vendor/counter-up/jquery.counterup.min.js">
     </script>
-    <script src="vendor/circle-progress/circle-progress.min.js"></script>
-    <script src="vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
-    <script src="vendor/chartjs/Chart.bundle.min.js"></script>
-    <script src="vendor/select2/select2.min.js">
+    <script src="<?php echo BASE_URL; ?>assets/vendor/circle-progress/circle-progress.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>assets/vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="<?php echo BASE_URL; ?>assets/vendor/chartjs/Chart.bundle.min.js"></script>
+    <script src="<?php echo BASE_URL; ?>assets/vendor/select2/select2.min.js">
     </script>
 
     <!-- Main JS-->
-    <script src="js/templa_adm.js"></script>
+    <script src="<?php echo BASE_URL; ?>assets/js/templa_adm.js"></script>
 
 </body>
 
